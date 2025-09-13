23056eaf2d2c27a459b75bff50e78a601df7bf52
Mickaël Bourgier <mickael.bourgier@gmail.com>
2025-09-13 22:23:41 +0200

    Fix and add tests to `InMemoryMessageRepository`

--- src/MessageRepository/InMemoryMessageRepository.php
+++ src/MessageRepository/InMemoryMessageRepository.php
@@ -36,29 +36,42 @@ final class InMemoryMessageRepository implements MessageRepositoryInterface
     #[\Override]
     public function findBy(FindByCriteria $criteria): FindResults
     {
-        $now = new \DateTimeImmutable();
-
-        $items = [];
-        $i = 0;
-        foreach ($this->messages as $message) {
-            if (null !== ($failoverAdapter = $criteria->getFailoverAdapter())) {
-                if ($message->getFailoverAdapter() !== $failoverAdapter) {
-                    continue;
-                }
-            }
-
-            $items[] = new MessageWithMetadata($message, $now, $now);
-
-            if (++$i > $criteria->getLimit()) {
-                break;
-            }
-        }
-
         return new FindResults(
             $criteria->getLimit(),
             count($this->messages),
             $criteria->getPage(),
-            $items
+            $this->iterateBy($criteria),
         );
+    }
+
+    /**
+     * @return iterable<int, MessageWithMetadata>
+     */
+    private function iterateBy(FindByCriteria $criteria): iterable
+    {
+        $now = new \DateTimeImmutable();
+
+        $i = 0; // index of the message in the complete array
+        $j = 0; // number of eligible messages (without considering page and limit)
+        $k = 0; // number of returned messages
+
+        while (isset($this->messages[$i]) && $k < $criteria->getLimit()) {
+            if (null !== $criteria->getFailoverAdapter() && $criteria->getFailoverAdapter() !== $this->messages[$i]->getFailoverAdapter()) {
+                ++$i;
+                continue;
+            }
+
+            if ($j < ($criteria->getPage() - 1) * $criteria->getLimit()) {
+                ++$i;
+                ++$j;
+                continue;
+            }
+
+            yield new MessageWithMetadata($this->messages[$i], $now, $now);
+
+            ++$i;
+            ++$j;
+            ++$k;
+        }
     }
 }
